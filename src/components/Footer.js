325b288133209cbfb0031153a3e38ac423a9b855
Ju7a <ju7a.d0nkey@gmail.com>
2020-04-23 04:15:26 +0300

    #content change #hide product page #add logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logo from '../img/logo-white.png'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+
 
 const Footer = class extends React.Component {
   render() {
@@ -14,8 +13,8 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="Supaminds"
+            style={{ height: '10em' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
@@ -34,16 +33,11 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <li>
+{/*                    <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
+                    </li>*/}
                     <li>
                       <a
                         className="navbar-item"
@@ -81,25 +75,10 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
                 <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
